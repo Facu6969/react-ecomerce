50b639c91fb834b7f1cc92662dfcc4cea0f2e830
facu-87 <facu.fernandez87@gmail.com>
2024-05-16 19:42:26 -0300

    PreEntrega1 Fernandez Facundo

---
+++ src/components/header/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className="carrito">
+      🛒 1
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Cart from './Cart'
+
+const NavBar = () => {
+  return (
+    <nav className='nav'>
+            <ul className='nav-menu'>
+                <li><a className='nav-link' href="#">Inicio</a></li>
+                <li><a className='nav-link' href="#">Buzos</a></li>
+                <li><a className='nav-link' href="#">Remeras</a></li>
+                <li><a className='nav-link' href="#">Musculosas</a></li>
+            </ul>
+     </nav>
+  )
+}
+
+export default NavBar
