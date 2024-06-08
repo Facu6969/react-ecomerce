0f275923c3d8fe65e5c25ed9384d8ea58dad3522
facu-87 <facu.fernandez87@gmail.com>
2024-06-08 20:14:42 -0300

    cambiento forma de linkear

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,12 +6,12 @@ const Item = ({ producto }) => {
     <div className='producto'>
       <Link to={`/item/${producto.id}`}>
         <h2>{producto.categoria.nombre}</h2>
-        <p>Medida: {producto.medida}</p>
+      </Link>
+      <p>Medida: {producto.medida}</p>
         <p>Largo: {producto.largo}</p>
         <p>Precio: ${producto.precio}</p>
         <p>Cantidad: {producto.cantidad}</p>
         <img src={producto.imagen} alt={producto.categoria.nombre} />
-      </Link>
     </div>
   );
 }
